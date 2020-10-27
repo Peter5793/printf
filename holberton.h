d681debf68e2181bf7977fdf6273e5106f3891ff
Faith-qa <atienofaith12@gmail.com>
2020-10-27 21:29:05 +0300

    prints based on format

--- holberton.h
+++ holberton.h
@@ -16,13 +16,25 @@ typedef struct specifier
   char spec;
   int (*function_ptr)(va_list);
 } specifier_t;
-
+int parser(const char *format, specifier function_list[], va_list lst);
 int _print(const char *format, ...);
 int _putchar(char c);
-int print_char(va_list all_parameters);
-int print_string(va_list all_parameters);
-int print_percent(va_list all_parameters);
-int print_integer(va_list all_parameters);
+int _write_char(char);
+int print_char(va_list);
+int print_string(va_list);
+int print_percent(va_list);
+int print_integer(va_list);
+int print_number(va_list);
+int print_binary(va_list);
+int print_reversed(va_list arg);
+int rot13(va_list);
+int unsigned_integer(va_list);
+int print_octal(va_list list);
+int print_hex(va_list list);
+int print_heX(va_list list);
+
+
+
 int print_unsign(int n);
 int counter(int j);
 int _puts(char *str);
