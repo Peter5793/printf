97dc6db1bd66beedcf43053e115ad342caecb7d3
Peter5793 <masaba.peter68@gmail.com>
2020-10-29 00:46:29 +0300

    test run

--- _printf.c
+++ _printf.c
@@ -10,7 +10,6 @@
 
 int _printf(const char *format, ...)
 {
-<<<<<<< HEAD
   /**points to an undefined number ofarguments*/
 	va_list arg_list;
 	int a, b, count = 0;
@@ -26,7 +25,7 @@ int _printf(const char *format, ...)
 			a++;
 		else
 		{
-			_putchar(formay[a]);
+			_putchar(format[a]);
 			a++;
 			count++;
 			continue;
@@ -43,42 +42,13 @@ int _printf(const char *format, ...)
 		a++;
 
 	}
-	/* cleans up the list*/
+
+	/* Cleans up the list */
 	va_end(arg_list);
-	/*Returns the character count here*/
+	/* Returns char count here */
 	return (count);
-=======
-	int b; /**printed char*/
-	specifier_t function_list[] = {
-		{"c", print_char},
-		{"s", print_string},
-		{"%", print_percent},
-		{"d", print_integer},
-		{"i", print_integer},
-		{"b", print_binary},
-		{"r", print_reversed},
-		{"R", rot13},
-		{"u", unsigned_integer},
-		{"o", print_octal},
-		{"x", print_hex},
-		{"X", print_heX},
-		{NULL, NULL}
-	};
-	va_list lst;
-
-	if (format == NULL)
-		return (-1);
-
-	va_start(lst, format);
-	/**calling the parser function*/
-
-	b = parser(format, function_list, lst);
-	va_end(lst);
-	return (b);
-
-
->>>>>>> 372da638c001b38a127b5b7d95826e1ff02d6e87
 }
+	
 /**
  *get_struct - gets the struct
  *@c: the char
@@ -89,13 +59,12 @@ int _printf(const char *format, ...)
 int get_struct(char c, va_list arg_list)
 {
 	specifier_t structs_ar[] = {
-		{'c', print_char},
-		{'s', print_string
-		}
-		{'%', print_percent}
-		{'d', print_integer}
-		{'i', print_integer}
-		{'\0', NULL}
+				    {'c', print_char},
+				    {'s', print_string},
+				    {'%', print_percent},
+				    {'d', print_integer},
+				    {'i', print_integer},
+		                    {'\0', NULL}
 	};
 	int b = 0;
 
@@ -106,5 +75,7 @@ int get_struct(char c, va_list arg_list)
 			return (structs_ar[b].function_ptr(arg_list));
 			b++;
 		}
-		return (-1);
+		
 	}
+	return (-1);
+}
