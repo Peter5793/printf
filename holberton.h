17cf6bafe1512f5b09f4c825ab975effbe7ac576
Faith-qa <atienofaith12@gmail.com>
2020-10-28 22:44:49 +0300

    headerfile

--- holberton.h
+++ holberton.h
@@ -1,42 +1,30 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
 
-/*typedef - for user defined data */
+/* typedef used to name user defined data types */
+
 /**
  * struct specifier - function that produces output according to a format
  *
- * @spec: print format
- * @function_ptr: function pointer to the specifier
+ * @spec: Format to print
+ * @function_ptr: The function pointer to the specifier
  */
-
 typedef struct specifier
 {
-  char spec;
-  int (*function_ptr)(va_list);
+	char spec;
+	int (*function_ptr)(va_list);
 } specifier_t;
-int parser(const char *format, specifier function_list[], va_list lst);
-int _print(const char *format, ...);
+
+int _printf(const char *format, ...);
 int _putchar(char c);
-int _write_char(char);
-int print_char(va_list);
-int print_string(va_list);
-int print_percent(va_list);
-int print_integer(va_list);
-int print_number(va_list);
-int print_binary(va_list);
-int print_reversed(va_list arg);
-int rot13(va_list);
-int unsigned_integer(va_list);
-int print_octal(va_list list);
-int print_hex(va_list list);
-int print_heX(va_list list);
-
-
-
+int print_char(va_list all_parameters);
+int print_string(va_list all_parameters);
+int print_percent(va_list all_parameters);
+int print_integer(va_list all_parameters);
 int print_unsign(int n);
 int counter(int j);
 int _puts(char *str);
 int get_struct(char c, va_list all_parameters);
-#endif /*HOLBERTON_H*/
+#endif /* HOLBERTON_H */
